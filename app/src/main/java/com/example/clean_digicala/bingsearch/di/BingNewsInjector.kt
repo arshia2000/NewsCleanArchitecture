5fce98377cec280bb5081ce95c9ab69bca3c5d63
arshia <arshia.vaezi200@yahoo.com>
2022-11-10 20:22:58 +0330

    add BingNewsInjector.kt

---
+++ app/src/main/java/com/example/clean_digicala/bingsearch/di/BingNewsInjector.kt
@@ -0,0 +1,10 @@
+package com.example.clean_digicala.bingsearch.di
+
+import com.example.clean_digicala.bingsearch.di.module.BingNewsModule
+
+object BingNewsInjector {
+
+
+    fun provideDependency()= listOf(BingNewsModule)
+
+}
