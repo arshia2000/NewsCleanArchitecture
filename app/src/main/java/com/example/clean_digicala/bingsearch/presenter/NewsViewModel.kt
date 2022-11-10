db6c1d9f3738553be0af869e3b2aca11445078a0
arshia <arshia.vaezi200@yahoo.com>
2022-11-10 20:33:21 +0330

    add NewsViewModel.kt

---
+++ app/src/main/java/com/example/clean_digicala/bingsearch/presenter/NewsViewModel.kt
@@ -0,0 +1,31 @@
+package com.example.clean_digicala.bingsearch.presenter
+
+import android.util.Log
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.clean_digicala.bingsearch.data.entity.BingNewsEntity
+import com.example.clean_digicala.bingsearch.domain.model.NewsModel
+import com.example.clean_digicala.bingsearch.domain.usecase.GetNewsList
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+class NewsViewModel( private var getNewsList: GetNewsList):ViewModel() {
+
+
+    var data= MutableLiveData<NewsModel>()
+
+
+    fun getNews(){
+
+       viewModelScope.launch(Dispatchers.IO) {
+
+
+          data.postValue(getNewsList.getNews())
+
+
+       }
+
+    }
+
+}
