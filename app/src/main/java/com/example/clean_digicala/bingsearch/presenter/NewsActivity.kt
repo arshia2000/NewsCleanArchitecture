480171f601b4de044b01effd6179057340d73e20
arshia <arshia.vaezi200@yahoo.com>
2022-11-10 20:30:54 +0330

    add NewsActivity.kt

---
+++ app/src/main/java/com/example/clean_digicala/bingsearch/presenter/NewsActivity.kt
@@ -0,0 +1,19 @@
+package com.example.clean_digicala.bingsearch.presenter
+
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.clean_digicala.R
+
+
+class NewsActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_news)
+
+
+    }
+
+}
