68d9af72990eb55e9dcc397949a786a17bd4d6ba
arshia <arshia.vaezi200@yahoo.com>
2022-11-12 22:54:21 +0330

    update BingApiService.kt

--- app/src/main/java/com/example/clean_digicala/bingsearch/data/api/BingApiService.kt
+++ app/src/main/java/com/example/clean_digicala/bingsearch/data/api/BingApiService.kt
@@ -4,12 +4,14 @@ import com.example.clean_digicala.bingsearch.data.entity.BingNewsEntity
 import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Headers
+import retrofit2.http.Query
 
 interface BingApiService {
 
+  //  /v2/everything?q=apple&from=2022-11-08&to=2022-11-08&sortBy=popularity&apiKey=34534b86d45c4ddc8672670d4f7f7590
 
-    @GET("/v2/everything?q=apple&from=2022-11-08&to=2022-11-08&sortBy=popularity&apiKey=34534b86d45c4ddc8672670d4f7f7590")
+    @GET("/v2/everything")
 
-    suspend fun getNews(): Response<BingNewsEntity>
+    suspend fun getNews(@Query("q") search: String,@Query("from") from: String,@Query("to") to: String,@Query("sortBy") sortBy: String,@Query("apiKey") apiKey:String): Response<BingNewsEntity>
 
 }
