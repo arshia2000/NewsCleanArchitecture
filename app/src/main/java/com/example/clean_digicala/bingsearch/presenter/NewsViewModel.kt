f4563a97bb7d273973210f18f19452a38369d1c7
arshia <arshia.vaezi200@yahoo.com>
2022-11-12 22:48:54 +0330

    Update NewsViewModel.kt

--- app/src/main/java/com/example/clean_digicala/bingsearch/presenter/NewsViewModel.kt
+++ app/src/main/java/com/example/clean_digicala/bingsearch/presenter/NewsViewModel.kt
@@ -1,12 +1,9 @@
 package com.example.clean_digicala.bingsearch.presenter
 
-import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.clean_digicala.bingsearch.data.entity.ArticlesItem
-import com.example.clean_digicala.bingsearch.data.entity.BingNewsEntity
-import com.example.clean_digicala.bingsearch.domain.model.NewsModel
 import com.example.clean_digicala.bingsearch.domain.usecase.GetNewsList
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -17,17 +14,23 @@ class NewsViewModel( private var getNewsList: GetNewsList):ViewModel() {
     var data= MutableLiveData<List<ArticlesItem>>()
 
 
-    fun getNews(){
+    fun getNews(searchQuery:String){
+
 
        viewModelScope.launch(Dispatchers.IO) {
 
 
-          val array= getNewsList.getNews().articles as List<ArticlesItem>
+          val array= getNewsList.getNews(searchQuery)?.articles as List<ArticlesItem>
           data.postValue(array)
 
 
-       }
 
+
+       }
     }
+
+
+
+
 
 }
