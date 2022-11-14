36023a3a0f082997ed0c24e5cf5d9b2344b979b6
arshia <arshia.vaezi200@yahoo.com>
2022-11-14 16:08:11 +0330

    update NewsModel.kt

--- app/src/main/java/com/example/clean_digicala/bingsearch/domain/model/NewsModel.kt
+++ app/src/main/java/com/example/clean_digicala/bingsearch/domain/model/NewsModel.kt
@@ -1,8 +1,11 @@
 package com.example.clean_digicala.bingsearch.domain.model
 
+import android.os.Parcelable
 import com.example.clean_digicala.bingsearch.data.entity.ArticlesItem
+import kotlinx.android.parcel.Parcelize
 
+@Parcelize
 class NewsModel(val totalResults: Int? = null,
                 val articles: List<ArticlesItem?>? = null,
-                val status: String? = null)
+                val status: String? = null) : Parcelable
 
